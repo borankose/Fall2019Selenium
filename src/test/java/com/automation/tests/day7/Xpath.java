1dae62aa002bc6827d2f806cc580cfe668077ca8
borankose <borankose37@gmail.com>
2020-03-21 14:08:28 -0700

    adding days

--- src/test/java/com/automation/tests/day7/Xpath.java
+++ src/test/java/com/automation/tests/day7/Xpath.java
@@ -7,13 +7,19 @@ import org.openqa.selenium.DeviceRotation;
 import org.openqa.selenium.WebDriver;
 
 public class Xpath {
-    static String userNameLocator = "//label[text()='Username']/following-sibling::input";
+  public  static String userNameLocator = "//label[text()='Username']/following-sibling::input";
+    public static String passwordLocator = "//label[text()='Password']/following-sibling::input";
 
     public static void main(String[] args) {
         WebDriver driver = DriverFactory.createDriver("chrome");
         driver.get("http://practice.cybertekschool.com/login");
         BrowserUtils.wait(3);
-        driver.findElement(By.xpath(userNameLocator));
+        driver.findElement(By.xpath(userNameLocator)).sendKeys("tomsmith");
+        driver.findElement(By.xpath(passwordLocator)).sendKeys("SuperSecretPassword");
+        BrowserUtils.wait(3);
+        driver.findElement(By.id("wooden_spoon")).click();
+        BrowserUtils.wait(2);
+
 
 
         BrowserUtils.wait(3);
